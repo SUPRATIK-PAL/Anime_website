51193de2988aad21966a22492bffcacc735fd5ba
supratik <supratikpal53@gmail.com>
2023-10-27 22:07:43 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import AnimeItem from "./Components/AnimeItem.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Components/HomePage.js";
+import Gallery from "./Components/Gallery.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/anime/:id" element={<AnimeItem/>}/>
+          <Route path="/character/:id" element={<Gallery/>}/>
+        </Routes>
+      
+      </BrowserRouter>
   );
 }
 
---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Roboto&family=Roboto+Slab:wght@600;800&display=swap');
+    body{
+        font-family: 'Poppins', sans-serif;
+        font-family: 'Roboto', sans-serif;
+        font-family: 'Roboto Slab', serif;
+    }
+
+`;
+
+export default GlobalStyle;
